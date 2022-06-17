8bf47af03053a8a6a194039e178b502d277aef86
choisungwook <kgg1959@naver.com>
2022-06-18 08:18:58 +0900

    cicd -> entrypoint 수정

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -e
 
 pip install -r requirements.txt
 mkdir -p reports
-python -m pytest --cov-report xml:./reports/cov.xml term --cov=./ tests/ --junitxml=./reports/report.xml
+python -m pytest --cov-report xml:./reports/cov.xml --cov=./ tests/ --junitxml=./reports/report.xml
