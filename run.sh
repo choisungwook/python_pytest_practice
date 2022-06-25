e47b531fb52a20f1469fe5db4d3e3420e55e18d2
choisungwook <kgg1959@naver.com>
2022-06-25 16:15:52 +0900

    sonarqube 보고서 push 설정

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 set -e
 
 pip install -r requirements.txt
+
+rm -rf ./reports
 mkdir -p reports
+
 python -m pytest --cov-report xml:./reports/cov.xml --cov=./ tests/ --junitxml=./reports/report.xml
